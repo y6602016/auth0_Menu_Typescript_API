535416758ba36db2f1c892da63f0d4d5548840c8
mike <y6602016@gmail.com>
2021-04-14 11:17:38 +0800

    add controller

--- src/items/items.router.ts
+++ src/items/items.router.ts
@@ -1,8 +1,7 @@
 // Required External Modules and Interfaces
 import express, { Request, Response, NextFunction } from "express";
-import * as ItemService from "./items.service";
-import { BaseItem, Item } from "./item.interface";
 import { checkJWT } from "../middleware/authz.middleware";
+import Controller from "../items/items.controller";
 
 // Router Definition
 export const itemsRouter = express.Router();
@@ -12,65 +11,12 @@ export const itemsRouter = express.Router();
 // GET and POST of "items/" route
 itemsRouter
   .route("/")
-  .get(async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const items: Item[] = await ItemService.findAll();
-      res.status(200).send(items);
-    } catch (e) {
-      res.status(500).send(e.message);
-    }
-  })
-  .post(checkJWT, async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const item: BaseItem = req.body;
-      const newItem = await ItemService.create(item);
-
-      res.status(200).json(newItem);
-    } catch (e) {
-      res.status(500).send(e.message);
-    }
-  });
+  .get(Controller.getItem)
+  .post(checkJWT, Controller.createItem);
 
 // GET and PUT and DELETE of "items/:id" route
 itemsRouter
   .route("/:id")
-  .get(async (req: Request, res: Response, next: NextFunction) => {
-    const id: number = parseInt(req.params.id, 10);
-
-    try {
-      const item: Item = await ItemService.find(id);
-      if (item) {
-        return res.status(200).send(item);
-      }
-      res.status(400).send("item not found");
-    } catch (e) {
-      res.status(500).send(e.message);
-    }
-  })
-  .put(checkJWT, async (req: Request, res: Response, next: NextFunction) => {
-    const id: number = parseInt(req.params.id, 10);
-
-    try {
-      const itemUpdate: BaseItem = req.body;
-      const existingItem: Item = await ItemService.find(id);
-      if (existingItem) {
-        const updatedItem = await ItemService.update(id, itemUpdate);
-        return res.status(200).json(updatedItem);
-      }
-      // if there is no existing item, create one
-      const newItem = await ItemService.create(itemUpdate);
-      res.status(200).json(newItem);
-    } catch (e) {
-      res.status(500).send(e.message);
-    }
-  })
-  .delete(checkJWT, async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const id: number = parseInt(req.params.id, 10);
-      await ItemService.remove(id);
-
-      res.sendStatus(204);
-    } catch (e) {
-      res.status(500).send(e.message);
-    }
-  });
+  .get(Controller.getDetail)
+  .put(checkJWT, Controller.updateDatail)
+  .delete(checkJWT, Controller.deleteDetail);
